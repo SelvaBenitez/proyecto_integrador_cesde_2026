874c3072ca5f309dcac2239d97610250d18f6c8e
SelvaBenitez <selva.benitezp05@gmail.com>
2026-03-03 18:48:06 -0500

    Creacion de getter & setter, constructor lleno y vacio

--- src/main/java/co/edu/cesde/pi/models/Users.java
+++ src/main/java/co/edu/cesde/pi/models/Users.java
@@ -6,4 +6,44 @@ public class Users {
     String password_hash;
     boolean status;
 
+    public Users(){}
+
+    public Users(long user_id, boolean status, String password_hash, String username) {
+        this.user_id = user_id;
+        this.status = status;
+        this.password_hash = password_hash;
+        this.username = username;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public String getPassword_hash() {
+        return password_hash;
+    }
+
+    public void setPassword_hash(String password_hash) {
+        this.password_hash = password_hash;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
